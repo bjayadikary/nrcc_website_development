c6b27c6140dbcf89ecfba91320b1a8cb83ff08a4
Jay Adhikari <bjayadikari.ba@gmail.com>
2023-03-06 16:52:04 +0545

    Added single-blog.html and Configured

--- nrccsite/nrccblog/urls.py
+++ nrccsite/nrccblog/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "nrccblog"
 urlpatterns = [
 	path("", views.blog, name="blog"),
+	path("details/", views.single_blog, name="single_blog"),
 ]
--- nrccsite/nrccblog/views.py
+++ nrccsite/nrccblog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def blog(request):
 	return render(request, "nrccblog/blog-page.html")
+
+def single_blog(request):
+	return render(request, "nrccblog/single-blog.html")
