915468e99be2478ad14a8758eaf66a601d052cd9
Jay Adhikari <bjayadikari.ba@gmail.com>
2023-03-08 17:52:12 +0545

    Worked Out on Models: Blogs, Authors, UserComments

--- nrccsite/nrccblog/admin.py
+++ nrccsite/nrccblog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Authors
+from .models import Authors, Blogs, UserComments
 
 # Register your models here.
 admin.site.register(Authors)
+admin.site.register(Blogs)
+admin.site.register(UserComments)
--- nrccsite/nrccblog/models.py
+++ nrccsite/nrccblog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 def validate_if_between_1_10(value):
@@ -8,12 +9,25 @@ def validate_if_between_1_10(value):
 		raise ValidationError("This field only accepts integers between 0 and 10")
 
 
+class Authors(models.Model):
+	author = models.CharField(max_length=100)
+	description = models.CharField(max_length=1000)
+	author_icon = models.ImageField(upload_to="images/test_uploads/", blank=True, null=True, default='default/no_image.jpg')
+	mail = models.EmailField(max_length=200)
+	joined_date = models.DateField()
+	priority = models.IntegerField(default=0, validators=[validate_if_between_1_10])
+
+
+	def __str__(self):
+		return f"{self.author}"
+
+
 class Blogs(models.Model):
+	author = models.ForeignKey(Authors, on_delete=models.PROTECT)
 	title = models.CharField("Blog Title", max_length=500)
 	body = models.TextField(default="Type here...")
-	author = models.CharField(max_length=100)
 	published_date = models.DateField(auto_now_add=True, blank=True)
-	updated_datetime = models.DateTimeField(auto_now_add=True, blank=True)
+	updated_datetime = models.DateTimeField(auto_now=True, blank=True)
 	priority = models.IntegerField(default=0, validators=[validate_if_between_1_10])
 	featured_image = models.ImageField(upload_to="images/test_uploads/", blank=True, null=True, default='default/no_image.jpg')
 
@@ -21,10 +35,13 @@ class Blogs(models.Model):
 		return f"{self.title}"
 
 
-class Authors(models.Model):
-	author = models.CharField(max_length=100)
-	description = models.CharField(max_length=1000)
-	author_icon = models.ImageField(upload_to="images/test_uploads/", blank=True, null=True, default='default/no_image.jpg')
-	mail = models.EmailField(max_length=200)
 
+class UserComments(models.Model):
+	blogs = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=200)
+	message = models.TextField(default="Here goes your Comments...")
+	date = models.DateField(auto_now_add=True, blank=True)
 
+	def __str__(self):
+		return f"Comments of {self.name}"
