c7b056d182d9c18451989da581309ed3454da2d4
Manoz7 <manojkhanal936@gmail.com>
2023-03-28 22:38:19 +0545

    [homepage] updated

--- nrccnepal/nrccnepal/urls.py
+++ nrccnepal/nrccnepal/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('superadmin/', admin.site.urls),
     path('', include('core.urls')),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls'), name='blog'),
 
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
