e6857b15a488b5c69b0ef798a1ef10f2d3ef0809
Jay Adhikari <Bijay Adhikari>
2023-04-03 21:24:48 +0545

    Templates

--- nrccsite/nrccblog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Authors, Blogs, UserComments
-
-# Register your models here.
-admin.site.register(Authors)
-admin.site.register(Blogs)
-admin.site.register(UserComments)
--- nrccsite/nrccblog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "nrccblog"
-urlpatterns = [
-	path("", views.blog, name="blog"),
-	path("details/", views.single_blog, name="single_blog"),
-]
